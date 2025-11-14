d4e9debf020b7d83468826b79a826be56e742f55
Mgalvaz <mgalvaz01@gmail.com>
2025-11-14 21:02:56 +0100

    Start training sequence

--- src/OCR_dataset.py
+++ src/OCR_dataset.py
@@ -14,120 +14,102 @@ Output:
     - y shape: (num_examples, 1, 7)
 """
 import random
-import os
-import warnings
-from sys import argv
 import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
+from torchvision.transforms import v2
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont, ImageEnhance
+from typing_extensions import deprecated
 
-def generate_plate_text():
+
+def generate_plate_text() -> str:
     nums = ''.join(random.choices('0123456789', k=4))
     letters = ''.join(random.choices('BCDFGHJKLMNPQRSTVWXYZ', k=3))
     return f'{nums}  {letters}'
 
-def add_gaussian_noise(img, mean=0, std=8):
+
+@deprecated('Use v2.GaussianNoise instead')
+def add_gaussian_noise(img: Image, mean: int = 0, std: int = 8) -> Image:
     arr = np.array(img).astype(np.float32)
     noise = np.random.normal(mean, std, arr.shape)
     arr_noisy = arr + noise
     arr_noisy = np.clip(arr_noisy, 0, 255)
     return Image.fromarray(arr_noisy.astype(np.uint8))
 
-def apply_perspective_transform(img, max_shift=3):
+
+@deprecated('Use v2.RandomPerspective instead')
+def apply_perspective_transform(img: Image.Image, max_shift: int = 3) -> Image:
     width, height = img.size
     src = np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype=np.float32)
     dst = src + np.random.randint(-max_shift, max_shift + 1, size=(4, 2))
 
-    def find_perspective_coeffs(src_points, dst_points):
+    def find_perspective_coeffs(src_points: np.ndarray, dst_points: np.ndarray) -> list[int]:
         matrix = []
         for p1, p2 in zip(dst_points, src_points):
             matrix.append([p1[0], p1[1], 1, 0, 0, 0, -p2[0] * p1[0], -p2[0] * p1[1]])
             matrix.append([0, 0, 0, p1[0], p1[1], 1, -p2[1] * p1[0], -p2[1] * p1[1]])
-        A = np.matrix(matrix, dtype=float)
+        A = np.matrix(matrix, dtype=np.float64)
         B = np.array(src_points).reshape(8)
-        res = np.dot(np.linalg.inv(A.T * A) * A.T, B)
-        return np.array(res).reshape(8)
+        res, _, _, _ = np.linalg.lstsq(A, B, rcond=None)
+        return res.tolist()
 
-    coeffs = find_perspective_coeffs(dst, src)
+    coeffs = find_perspective_coeffs(src, dst)
     return img.transform(img.size, Image.PERSPECTIVE, coeffs, fillcolor=100)
 
-def random_brightness_contrast(img, brightness_range=(0.8,1.2), contrast_range=(0.8,1.2)):
+@deprecated('Use v2.ColorJitter instead')
+def random_brightness_contrast(img: Image, brightness_range: tuple = (0.8, 1.2), contrast_range: tuple = (0.8, 1.2)) -> Image:
     enhancer = ImageEnhance.Brightness(img)
     img = enhancer.enhance(np.random.uniform(*brightness_range))
     enhancer = ImageEnhance.Contrast(img)
     img = enhancer.enhance(np.random.uniform(*contrast_range))
     return img
 
-def augment_image(img):
-    img = img.rotate(np.random.uniform(-5, 5), expand=False, fillcolor=100) # Rotation
-    img = apply_perspective_transform(img) # Transformation to simulate depth
-    img = add_gaussian_noise(img) # Noise
-    img = random_brightness_contrast(img) # Change brightness
+
+@deprecated('Use augment_image_v2 instead')
+def augment_image(img: Image) -> Image:
+    img = img.rotate(np.random.uniform(-5, 5), expand=False, fillcolor=100)  # Rotation
+    img = apply_perspective_transform(img)  # Transformation to simulate depth
+    img = add_gaussian_noise(img)  # Noise
+    img = random_brightness_contrast(img)  # Change brightness
     return img
 
 
-class SyntheticPlateDataset(Dataset):
-    def __init__(self, num_samples=10000, transform=None, precomputed_file=None):
-        super().__init__()
-        if precomputed_file and os.path.exists(precomputed_file):
-            self.use_precomputed = True
-            self.imgs, self.labels = torch.load(precomputed_file, weights_only=True)
-            if num_samples > len(self.labels):
-                warnings.warn(
-                    f"\nWARNING: Received {num_samples} samples, "
-                    f"but precomputed file only has {len(self.labels)}.\n"
-                    f"Proceeding with {len(self.labels)} samples.\n",
-                    UserWarning
-                )
-                self.num_samples = len(self.labels)
-            else:
-                self.num_samples = num_samples
-        else:
-            self.use_precomputed = False
-            self.num_samples = num_samples
-            self.font_main = ImageFont.truetype('arial.ttf', 15)
-            self.font_small = ImageFont.truetype('arial.ttf', 5)
-            self.transform = transform or transforms.ToTensor()
-            self.translator = dict((l, n) for n, l in enumerate('BCDFGHJKLMNPQRSTVWXYZ0123456789', start=1))
+def augment_image_v2(img: Image.Image) -> torch.Tensor:
+    tr = v2.Compose([
+        v2.PILToTensor(),
+        v2.ToDtype(torch.float, True),
+        v2.RandomRotation(degrees=5, fill=0.392),
+        v2.RandomPerspective(distortion_scale=0.20, p=1.0, fill=0.392),
+        v2.GaussianNoise(sigma=0.03),
+        v2.RandomApply([v2.ColorJitter(brightness=(0.8, 1.2), contrast=(0.8, 1.2))], p=0.7),
+    ])
+    return tr(img)
 
-    def __len__(self):
+
+class SyntheticPlateDataset(Dataset):
+    """
+    Dataset that generates fake synthetic spanish license plates and augments them to simulate perspective.
+    """
+
+    def __init__(self, num_samples: int = 10000):
+        super().__init__()
+        self.num_samples = num_samples
+        self.font_main = ImageFont.truetype('arial.ttf', 15)
+        self.font_small = ImageFont.truetype('arial.ttf', 5)
+        self.transform = augment_image_v2
+        self.translator = dict((l, n) for n, l in enumerate('BCDFGHJKLMNPQRSTVWXYZ0123456789', start=1))
+
+    def __len__(self) -> int:
         return self.num_samples
 
-    def __getitem__(self, idx):
-        if self.use_precomputed:
-            return self.imgs[idx], self.labels[idx]
-        else:
-            plate_text = generate_plate_text()
-
-            img = Image.new("L", (94, 20), color=100)
-            draw = ImageDraw.Draw(img)
-            draw.rectangle([4, 2, 16, 19], fill=55)
-            draw.rectangle([16, 2, 91, 19], fill=230)
-            draw.text((8, 12), 'E', font=self.font_small, fill=230)
-            draw.text((18, 2), plate_text, font=self.font_main, fill=50)
-            img = augment_image(img)
-            img = self.transform(img)
-            label = torch.tensor([self.translator[l] for l in plate_text if l != ' '], dtype=torch.long)
-
-            return img, label
-
-
-num_examples = int(argv[1]) if len(argv) > 1 and argv[1].isnumeric() else 10000
-dataset = SyntheticPlateDataset(num_samples=num_examples)
-
-X = torch.empty((len(dataset), 1, 20, 94))
-Y = []
-
-for i in range(len(dataset)):
-    x, y = dataset[i]
-    X[i] = x
-    Y.append(y)
-
-os.makedirs('datasets', exist_ok=True)
-torch.save((X,Y), 'datasets/ocr_dataset.pt')
-
-
-
-
+    def __getitem__(self, idx: int) -> tuple:
+        plate_text = generate_plate_text()
+        plate = Image.new("L", (91, 18), color=230)
+        draw = ImageDraw.Draw(plate)
+        draw.rectangle([0, 0, 12, 18], fill=55)
+        draw.text((4, 9), 'E', font=self.font_small, fill=230)
+        draw.text((15, 1), plate_text, font=self.font_main, fill=50)
+        plate = self.transform(plate)
+        label = torch.tensor([self.translator[l] for l in plate_text if l != ' '], dtype=torch.long)
+        return plate, label
--- src/app.py
+++ src/app.py
@@ -6,7 +6,12 @@ import torch.optim as optim
 from torch.nn import Sequential
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader, random_split
+from PIL import Image
+from torchvision.transforms import transforms, v2
+
 from OCR_dataset import SyntheticPlateDataset
+
+TRANSLATOR = dict((l, n) for n, l in enumerate('BCDFGHJKLMNPQRSTVWXYZ0123456789', start=1))
 
 def collate_fn(batch: list):
     imgs, labels = zip(*batch)
@@ -29,28 +34,33 @@ class MyModel(nn.Module):
         super(MyModel, self).__init__()
 
         self.cnn = Sequential(
-            nn.Conv2d(1, 32, (3,3), padding=1),
+            nn.Conv2d(1, 128, (3,3), padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128, 256, (3, 3), padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
-            nn.Conv2d(32, 64, (3, 3), padding=1),
+            nn.Conv2d(256, 256, (3, 3), padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2)
+            nn.Conv2d(256, 512, (3, 3), padding=1),
+            nn.ReLU()
         )
 
-        self.rnn = nn.GRU(320, 64, batch_first=True, bidirectional=True) #320 = 64 * 5 = (channels * height) as if it were reading the image left to right
+        self.rnn = nn.GRU(4608, 64, batch_first=True, bidirectional=True) #256 = 64 * 8 = (channels * height) as if it were reading the image left to right
 
-        self.fc = nn.Linear(128, 10) #128 = 64 * 2 because GRU is bidirectional
+        self.fc = nn.Linear(128, 32) #128 = 64 * 2 because GRU is bidirectional, 32 = num_classes + 1 (for <blank>)
 
     def forward(self, x):
         _, ic, ih, iw = x.size()
-        assert (ic, ih, iw) == (1, 20, 94), f'Input size ({ic}, {ih}, {iw}) does not correspond to expected size (1, 20, 94)'
+        assert (ic, ih, iw) == (1, 18, 91), f'Input size ({ic}, {ih}, {iw}) does not correspond to expected size (1, 18, 91)'
         x = self.cnn(x)
+        #print('CNN', x.min(), x.max())
 
         b, c, h, w = x.size()
         x = x.permute(0, 3, 1, 2)  # (batch, width, channels, height)
         x = x.reshape(b, w, c * h)  # (batch, seq_len, input_size)
 
         x, _ = self.rnn(x)
+        #print('RNN', x.min(), x.max())
         x = self.fc(x)
         return x.log_softmax(2)
 
@@ -60,7 +70,8 @@ if __name__ == "__main__":
     total = 700
     num_test = total//7
     num_train = total - num_test
-    dataset = SyntheticPlateDataset(num_samples=total, precomputed_file='../datasets/ocr_dataset.pt')
+    num_epochs = 1
+    dataset = SyntheticPlateDataset(num_samples=total)
     train_dataset, test_dataset = random_split(dataset, [num_train, num_test])
 
     train_loader = DataLoader(train_dataset, batch_size=64, collate_fn=collate_fn)
@@ -73,23 +84,26 @@ if __name__ == "__main__":
 
 
     #Training
-    iteration = 1
-    num_iterations = len(train_loader)
-    for batch_images, batch_labels in train_loader:
-        print(f'iteration: {iteration} out of {num_iterations}')
-        iteration += 1
-        optimizer.zero_grad()
+    for epoch in range(num_epochs):
+        print(f'Epoch {epoch+1} out of {num_epochs}')
+        iteration = 1
+        num_iterations = len(train_loader)
+        for batch_images, batch_labels in train_loader:
+            print(f'iteration: {iteration} out of {num_iterations}')
+            iteration += 1
+            optimizer.zero_grad()
 
-        batch_output = model(batch_images)
-        log_probs = batch_output.permute(1, 0, 2)
-        input_lengths = torch.full(size=(batch_output.size(0),), fill_value=batch_output.size(1), dtype=torch.long)
-        batch_labels = [lbl[lbl != -1] for lbl in batch_labels] # Remove -1 padding
-        targets = torch.cat(batch_labels)
-        target_lengths = torch.tensor([len(l) for l in batch_labels], dtype=torch.long)
+            batch_output = model(batch_images)
+            log_probs = batch_output.permute(1, 0, 2)
+            input_lengths = torch.full(size=(batch_output.size(0),), fill_value=batch_output.size(1), dtype=torch.long)
+            batch_labels = [lbl[lbl != -1] for lbl in batch_labels] # Remove -1 padding
+            targets = torch.cat(batch_labels)
+            target_lengths = torch.tensor([len(l) for l in batch_labels], dtype=torch.long)
 
-        loss = criterion(log_probs, targets, input_lengths, target_lengths)
-        loss.backward()
-        optimizer.step()
+            loss = criterion(log_probs, targets, input_lengths, target_lengths)
+            loss.backward()
+            optimizer.step()
+        print()
 
 
     #Testing
@@ -107,5 +121,26 @@ if __name__ == "__main__":
                 target = target.cpu().numpy().tolist()
                 if pred == target:
                     correct += 1
-
     print(f"Exact match accuracy: {correct / num_test:.2%}")
+
+
+    input('Predecir la imagen')
+    transform = transforms.Compose([
+        v2.Grayscale(num_output_channels=1),
+        v2.Resize((18, 91)),
+        v2.PILToTensor(),
+        v2.ToDtype(torch.float, True),
+    ])
+
+    with torch.no_grad():
+        image = Image.open('src/3245_LCX.png')
+        #image.show()
+        #input('siguiente')
+        ten = transform(image)
+        ten = ten.unsqueeze(0)
+        #v2.ToPILImage()(ten).show()
+        #input('siguiente')
+        out = model(ten)
+        pred = out.argmax(dim=2).cpu().numpy().T
+        decoded_preds = ctc_decode(pred)
+        print(decoded_preds)
