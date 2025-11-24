605c75d7783770ae73f3df07463349373119e17b
Mgalvaz <mgalvaz01@gmail.com>
2025-11-24 20:41:30 +0100

    Start the ALPR neural network

--- src/train_LPD_model.py
+++ src/train_LPD_model.py
@@ -20,13 +20,17 @@ Output:
     }, out)
 """
 import argparse
+from typing import Any
+
+import numpy as np
 import torch
 from torch import nn
+from torchvision.models.detection.anchor_utils import AnchorGenerator
 
 
 class InceptionResidual(nn.Module):
 
-    def __init__(self, inc: int, outc: int, *args: tuple[tuple[int, int] | int, ...], include_max_pool: bool =True) -> None:
+    def __init__(self, inc: int, outc: int, *args: tuple[int, int] | int, include_max_pool: bool =True) -> None:
         super().__init__()
 
         # Inception module
@@ -38,7 +42,7 @@ class InceptionResidual(nn.Module):
                 pad = kernel // 2
             else:
                 pad = (kernel[0] // 2, kernel[1] // 2)
-            self.branches.append(
+            self.inception.append(
                 nn.Sequential(
                     nn.Conv2d(inc, outc_hidden, kernel, padding=pad),
                     nn.BatchNorm2d(outc_hidden),
@@ -53,7 +57,7 @@ class InceptionResidual(nn.Module):
                 nn.ReLU()
             ))
         # Conv2D for the inception module
-        self.final_conv =  nn.Conv2d(outc, outc, 1)
+        self.final_conv =  nn.Conv2d(outc, outc, 1, bias=False)
         # Decoder
         self.decoder = nn.ReLU()
 
@@ -72,27 +76,67 @@ class InceptionResidual(nn.Module):
         out = self.decoder(out)
         return out
 
-class CRNN(nn.Module):
+class Prediction(nn.Module):
 
-    def __init__(self):
+    def __init__(self, inc: int) -> None:
+        super().__init__()
+        self.cls = nn.Conv2d(inc, 2, kernel_size=3, padding=1) # (32, 16, 16) -> (2, 16, 16) or (48, 8, 8) -> (2, 8, 8)
+        self.reg = nn.Conv2d(inc, 4, kernel_size=3, padding=1) # (32, 16, 16) -> (4, 16, 16) or (48, 8, 8) -> (4, 8, 8)
+
+    def forward(self, x:torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        cls = self.cls(x)
+        reg = self.reg(x)
+        return cls, reg
+
+class ALPR(nn.Module):
+
+    def __init__(self) -> None:
         super().__init__()
 
-        self.cnn = nn.Sequential(
-            nn.Conv2d(1, 16, 5, padding=4) # (1, 128, 128) -> (16, )
-            InceptionResidual(16, 24, 6, 4, 2, include_max_pool=True) #()
+        self.block1 = nn.Sequential(
+            nn.Conv2d(3, 16, 5, padding=2), # (3, 384, 384) -> (16, 128, 128)
+            InceptionResidual(16, 16, 5, 3, 2, include_max_pool=True), #(16, 128, 128)
+        )
+        self.block2 = nn.Sequential(
+            nn.Conv2d(16, 24, 3, padding=1),  # (16, 128, 128) -> (24, 64, 64)
+            InceptionResidual(24, 24, 5, 3, 3, include_max_pool=True), # (24, 64, 64)
+        )
+        self.block3 = nn.Sequential(
+            nn.Conv2d(24, 32, 3, padding=1),  # (24, 128, 128) -> (32, 32, 32)
+            InceptionResidual(32, 32, 5, 3, 3, include_max_pool=True) , # (32, 32, 32) -> (32, 32, 32)
+        )
+        self.block4 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=1),  # (32, 32, 32) -> (32, 16, 16)
+            InceptionResidual(32, 32, 5, 3, 3, include_max_pool=True),  # (32, 16, 16) -> (32, 16, 16)
+        )
+        self.block5 = nn.Sequential(
+            nn.Conv2d(32, 48, 3, padding=1),  # (32, 16, 16) -> (48, 8, 8)
+            InceptionResidual(48, 48, 5, 3, 3, include_max_pool=True)  # (48, 8, 8) -> (48, 8, 8)
         )
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        _, ic, ih, iw = x.size() # (batch, channels=1, height=32, width=150)
-        assert (ic, ih, iw) == (1, 32, 150), f'Input size ({ic}, {ih}, {iw}) does not correspond to expected size (1, 32, 150)'
-        x = self.cnn(x) # (batch, channels=512, height=1, width=34)
+        self.FM0 = Prediction(32) #(32, 16, 16) -> ((2, 16, 16) # matricula/no matricula, (4, 16, 16) # (dx, dy, dw, dh))
+        self.FM1 = Prediction(48) #(48, 8, 8) -> ((2, 8, 8)# matricula/no matricula, (4, 8, 8) # (dx, dy, dw, dh))
 
-        x = x.squeeze(2).permute(0, 2, 1)  # (batch, width=34, channels=512)
+        self.anchors = AnchorGenerator(sizes=((39,), (79,)), aspect_ratios=((2.0,), (2.0,))
+    )
 
-        x, _ = self.rnn(x) # (batch, seq_len=34, channels=512)
+    def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor, Any]:
+        images = x
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        fm0 = self.block4(x)
+        fm1 = self.block5(fm0)
 
-        x = self.decoder(x) # (batch, seq_len=34, label=37)
-        return x
+        anchors = self.anchors(images, [fm0, fm1])
+
+        cls0, reg0 = self.FM0(fm0)
+        cls1, reg1 = self.FM1(fm1)
+
+        cls = torch.cat([cls0, cls1], dim=1)
+        reg = torch.cat([reg0, reg1], dim=1)
+
+        return cls, reg, anchors
 
 
 
@@ -105,104 +149,11 @@ def main():
     parser.add_argument('--output-path', type=str, default='models/OCR_model.pth', help='Path to save the trained model (.pth)')
     args = parser.parse_args()
 
-    # Load dataset
-    num_test = args.total//7
-    num_train = args.total - num_test
-    dataset = SyntheticPlateDataset(num_samples=args.total)
-    train_dataset, test_dataset = random_split(dataset, [num_train, num_test])
+    model = ALPR()
+    dummy = torch.rand((3, 384, 384))
+    dummy = dummy.unsqueeze(0)
+    cls, reg, anch = model(dummy)
 
-    train_loader = DataLoader(train_dataset, batch_size=64, collate_fn=collate_fn)
-    test_loader = DataLoader(test_dataset, batch_size=64,  collate_fn=collate_fn)
-
-    for img, _ in train_loader:
-        pass
-
-    # Model loading
-    device = torch.device('cpu')
-    model = CRNN()
-    criterion = nn.CTCLoss(blank=0, zero_infinity=True, reduction='sum')
-    optimizer = optim.Adam(model.parameters(), lr=0.00001)
-
-    if args.model_path:
-        print(f'Loading model from {args.model_path}')
-        checkpoint = torch.load(args.model_path, weights_only=True, map_location=device)
-        model.load_state_dict(checkpoint['model_state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        last_epoch = checkpoint['epoch']
-        loss = checkpoint['loss']
-        print(f'Checkpoint loaded. Last epoch: {last_epoch}, with loss: {loss}')
-    else:
-        last_epoch = 0
-
-
-    # Training
-    if args.total > 0:
-        model.train()
-        num_epochs = args.epochs + last_epoch
-        for epoch in range(last_epoch+1, num_epochs+1):
-            print(f'Epoch {epoch}/{num_epochs}', end=' ')
-            for batch_images, batch_labels in train_loader:
-                # zero the parameter gradients
-                optimizer.zero_grad()
-                # forward
-                batch_output = model(batch_images)
-                log_probs = F.log_softmax(batch_output, dim=2).permute(1, 0 ,2)
-                input_lengths = torch.full(size=(batch_output.size(0),), fill_value=batch_output.size(1), dtype=torch.long)
-                targets = batch_labels
-                batch_labels = [lbl[lbl != -1] for lbl in batch_labels]
-                target_lengths = torch.tensor([len(l) for l in batch_labels], dtype=torch.long)
-                loss = criterion(log_probs, targets, input_lengths, target_lengths)
-                # backward
-                loss.backward()
-                torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-                # optimize
-                optimizer.step()
-            print(f'loss: {loss.item()}')
-
-        #Testing
-        model.eval()
-        correct = 0
-        with torch.no_grad():
-            for batch_images, batch_labels in test_loader:
-                output = model(batch_images).permute(1, 0 ,2)
-                preds = output.argmax(dim=2).cpu().numpy().T
-
-                batch_labels = [lbl[lbl != -1] for lbl in batch_labels]
-                decoded_preds = [ctc_decode(seq) for seq in preds]
-
-                for pred, target in zip(decoded_preds, batch_labels):
-                    target = target.cpu().numpy().tolist()
-                    if pred == target:
-                        correct += 1
-            print(f"Exact match accuracy: {correct / num_test:.2%}")
-
-
-    input('Predecir la imagen')
-    transform = transforms.Compose([
-        v2.Grayscale(num_output_channels=1),
-        v2.Resize((32, 150)),
-        v2.PILToTensor(),
-        v2.ToDtype(torch.float, True),
-    ])
-
-    with torch.no_grad():
-        image = Image.open('src/3245_LCX.png')
-        ten = transform(image)
-        v2.ToPILImage()(ten.squeeze(0)).show()
-        ten = ten.unsqueeze(0)
-        input('siguiente')
-        out = model(ten)
-        pred = out.argmax(dim=2).cpu().numpy().T
-        decoded_preds = ctc_decode(pred)
-        print([REVERSE_TRANSLATOR[n[0]] for n in decoded_preds])
-
-
-    """torch.save({
-        'epoch': epoch,
-        'model_state_dict': model.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict(),
-        'loss': loss.item(),
-    }, args.output_path)"""
 
 
 if __name__ == "__main__":
